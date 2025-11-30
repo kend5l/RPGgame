22110d13f3ebaae29f8505b2cd21203ccac835b9
kend5l <kendalkyle50@gmail.com>
2025-11-30 12:40:04 -0600

    player movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+
+using namespace sf;
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    RenderWindow window(VideoMode({ 1080, 720 }), "RPG Game");
+    Texture playerTexture;
+    playerTexture.loadFromFile("assets/player/textures/Idle__001.png");
+    Sprite playerSprite(playerTexture);
 
     while (window.isOpen())
     {
+        
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
@@ -15,7 +22,23 @@ int main()
             }
         }
 
+        Vector2f pos = playerSprite.getPosition();
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+            playerSprite.setPosition(pos + Vector2f(1, 0));
+        
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+            playerSprite.setPosition(pos + Vector2f(-1, 0));
+        
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+            playerSprite.setPosition(pos + Vector2f(0, -1));
+        
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+            playerSprite.setPosition(pos + Vector2f(0, 1));
+       
+
         window.clear();
+        window.draw(playerSprite);
         window.display();
     }
 }
